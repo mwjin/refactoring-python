b163295c969e8d00ce8c9507d639d013165a8776
Minwoo Jeong <jeongmwj@gmail.com>
2021-11-16 10:00:36 +0900

    Change the declaration of `get_amount_for`

--- Chapter01/statement.py
+++ Chapter01/statement.py
@@ -8,7 +8,7 @@ def statement(invoice: dict, plays: dict) -> str:
     dollar_format = "${:,.2f}".format
 
     for perf in invoice["performances"]:
-        this_amount = get_amount_for(perf, get_play_for(perf, plays))
+        this_amount = get_amount_for(perf, plays)
         volume_credits += max(perf["audience"] - 30, 0)
         if get_play_for(perf, plays)["type"] == "comedy":
             volume_credits += math.floor(perf["audience"] / 5)
@@ -24,24 +24,25 @@ def statement(invoice: dict, plays: dict) -> str:
     return result
 
 
-def get_play_for(performance: dict, plays: dict) -> dict:
-    return plays[performance["playID"]]
-
-
-def get_amount_for(performance: dict, play: dict) -> int:
+def get_amount_for(performance: dict, plays: dict) -> int:
     result = 0
 
-    if play["type"] == "tragedy":
+    if get_play_for(performance, plays)["type"] == "tragedy":
         result = 40000
         if performance["audience"] > 30:
             result += 1000 * (performance["audience"] - 30)
-    elif play["type"] == "comedy":
+    elif get_play_for(performance, plays)["type"] == "comedy":
         result = 30000
         if performance["audience"] > 20:
             result += 10000 + 500 * (performance["audience"] - 20)
         result += 300 * performance["audience"]
     else:
-        raise ValueError(f'Unknown genre: {play["type"]}')
+        raise ValueError(
+            f'Unknown genre: {get_play_for(performance, plays)["type"]}'
+        )
 
     return result
 
+
+def get_play_for(performance: dict, plays: dict) -> dict:
+    return plays[performance["playID"]]
