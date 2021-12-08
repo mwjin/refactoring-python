0bfd9d220726b44ed39e65dd18949d0407f61efc
Minwoo Jeong <jeongmwj@gmail.com>
2021-12-08 15:35:38 +0900

    Add `asia` fixture

--- Chapter04/test_province.py
+++ Chapter04/test_province.py
@@ -1,11 +1,16 @@
+import pytest
+
 from province import Province, sample_province_data
 
 
-def test_province_short_fall():
-    asia = Province(sample_province_data())
+@pytest.fixture(scope="function")
+def asia():
+    return Province(sample_province_data())
+
+
+def test_province_short_fall(asia):
     assert asia.short_fall == 5
 
 
-def test_province_profit():
-    asia = Province(sample_province_data())
+def test_province_profit(asia):
     assert asia.profit == 230
