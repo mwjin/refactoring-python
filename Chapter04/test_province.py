82eaa61fab76506716c3ca52f6cc90db31a7db6a
Minwoo Jeong <jeongmwj@gmail.com>
2021-12-08 15:31:27 +0900

    Add `test_province_profit`

--- Chapter04/test_province.py
+++ Chapter04/test_province.py
@@ -4,3 +4,8 @@ from province import Province, sample_province_data
 def test_province_short_fall():
     asia = Province(sample_province_data())
     assert asia.short_fall == 5
+
+
+def test_province_profit():
+    asia = Province(sample_province_data())
+    assert asia.profit == 230
