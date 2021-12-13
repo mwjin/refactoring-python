9b3160fab55534b2760c8f2c6b42e437d95f000d
Minwoo Jeong <jeongmwj@gmail.com>
2021-12-13 23:54:44 +0900

    Add `invoice` fixture

--- Chapter06/01-extract-function/tests.py
+++ Chapter06/01-extract-function/tests.py
@@ -1,16 +1,30 @@
 from datetime import date, timedelta
 
+import pytest
 from example import print_owing
+
+from invoice import Invoice
+from order import Order
+
+
+@pytest.fixture
+def invoice():
+    _invoice = Invoice("Minwoo Jeong")
+    for amount in range(10, 60, 10):
+        _invoice.add_order(Order(amount))
+    due_date = date.today() + timedelta(days=30)
+    _invoice.due_date = due_date
+
+    return _invoice
 
 
 def test_print_owing(capsys, invoice):
-    due_date = date.today() + timedelta(days=30)
     print_owing(invoice)
     expected = (
         "**** Customer Outstanding Debt ****\n",
         f"Customer: {invoice.customer}\n",
         "Outstanding Debt: 150\n",
-        f"Due Date: {due_date.strftime('%d/%m/%y')}\n",
+        f"Due Date: {invoice.due_date.strftime('%d/%m/%y')}\n",
     )
     captured = capsys.readouterr()
     assert captured.out == expected
