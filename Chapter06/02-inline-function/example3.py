f4855d097822884854c7317f9a18b7ab50fcc326
Minwoo Jeong <jeongmwj@gmail.com>
2021-12-14 23:42:12 +0900

    Inline the first statement of the function

--- Chapter06/02-inline-function/example3.py
+++ Chapter06/02-inline-function/example3.py
@@ -1,9 +1,9 @@
 def report_lines(customer):
     lines = []
+    lines.append(["name", customer.name])
     gather_customer_data(lines, customer)
     return lines
 
 
 def gather_customer_data(out, customer):
-    out.append(["name", customer.name])
     out.append(["location", customer.location])
