5b18d03e947d97b56c5160c066b30fc6d68f7dd9
Minwoo Jeong <jeongmwj@gmail.com>
2021-12-14 23:38:48 +0900

    Add a test for `report_lines` function

--- Chapter06/02-inline-function/tests.py
+++ Chapter06/02-inline-function/tests.py
@@ -1,7 +1,10 @@
+import pytest
+from example3 import report_lines
+
+from customer import Customer
+from driver import Driver
 from example1 import rating as rating1
 from example2 import rating as rating2
-
-from driver import Driver
 
 
 def get_driver(num_late_delivery):
@@ -9,6 +12,11 @@ def get_driver(num_late_delivery):
     for _ in range(num_late_delivery):
         driver.do_late_delivery()
     return driver
+
+
+@pytest.fixture
+def customer():
+    return Customer("Minwoo Jeong", "South Korea")
 
 
 def test_rating1():
@@ -21,3 +29,10 @@ def test_rating2():
     assert rating2(get_driver(10)) == 2
     assert rating2(get_driver(5)) == 1
     assert rating2(get_driver(1)) == 1
+
+
+def test_report_lines(customer):
+    assert report_lines(customer) == [
+        ["name", "Minwoo Jeong"],
+        ["location", "South Korea"],
+    ]
