70a7a37cc8e4da031e18859cb2f604a37e3cb4de
Minwoo Jeong <jeongmwj@gmail.com>
2021-12-14 21:27:29 +0900

    Extract the `print_details` function
    This is an example of extracting a code block with local variables

--- Chapter06/01-extract-function/example.py
+++ Chapter06/01-extract-function/example.py
@@ -14,11 +14,14 @@ def print_owing(invoice):
     today = date.today()
     invoice.due_date = today + timedelta(days=30)
 
-    # Print the details
-    print(f"Customer: {invoice.customer}")
-    print(f"Outstanding Debt: {outstanding}")
-    print(f"Due Date: {invoice.due_date.strftime('%d/%m/%y')}")
+    print_details(invoice, outstanding)
 
 
 def print_banner():
     print("**** Customer Outstanding Debt ****")
+
+
+def print_details(invoice, outstanding):
+    print(f"Customer: {invoice.customer}")
+    print(f"Outstanding Debt: {outstanding}")
+    print(f"Due Date: {invoice.due_date.strftime('%d/%m/%y')}")
