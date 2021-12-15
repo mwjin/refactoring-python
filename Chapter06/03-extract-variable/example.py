b1eba6df4719296a1d777bb27f4bf044d60271c6
Minwoo Jeong <jeongmwj@gmail.com>
2021-12-15 09:46:18 +0900

    Extract `shipping` variable
    and remove the redundant comment

--- Chapter06/03-extract-variable/example.py
+++ Chapter06/03-extract-variable/example.py
@@ -1,7 +1,5 @@
 def price(order):
-    # price = base price - quantity discount + shipping
     base_price = order.quantity * order.item_price
     quantity_discount = max(0, order.quantity - 500) * order.item_price * 0.05
-
-    return base_price - quantity_discount + min(base_price * 0.1, 100)
-
+    shipping = min(base_price * 0.1, 100)
+    return base_price - quantity_discount + shipping
