6b6b707d4eb5c50de93d09bcdda77afa90838c2e
Minwoo Jeong <jeongmwj@gmail.com>
2021-12-15 11:01:48 +0900

    Extract a temporary function from `add_reservation` method
    This is an example of changing function declaration using migration

--- Chapter06/05-change-function-declaration/book.py
+++ Chapter06/05-change-function-declaration/book.py
@@ -3,6 +3,9 @@ class Book:
         self._reservations = []
 
     def add_reservation(self, customer):
+        self.zz_add_reservation(customer)
+
+    def zz_add_reservation(self, customer):
         self._reservations.append(customer)
 
     @property
