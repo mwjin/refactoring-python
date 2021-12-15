5bd28d03e7807a1878072a1af40c6cf5fe2f4159
Minwoo Jeong <jeongmwj@gmail.com>
2021-12-15 09:54:42 +0900

    Extract methods from the `price` property

--- Chapter06/03-extract-variable/order.py
+++ Chapter06/03-extract-variable/order.py
@@ -13,8 +13,17 @@ class Order:
 
     @property
     def price(self):
-        return (
-            self.quantity * self.item_price
-            - max(0, self.quantity - 500) * self.item_price * 0.05
-            + min(self.quantity * self.item_price * 0.1, 100)
-        )
+        return self.base_price - self.quantity_discount + self.shipping
+
+    @property
+    def base_price(self):
+        return self.quantity * self.item_price
+
+    @property
+    def quantity_discount(self):
+        return max(0, self.quantity - 500) * self.item_price * 0.05
+
+    @property
+    def shipping(self):
+        return min(self.base_price * 0.1, 100)
+
