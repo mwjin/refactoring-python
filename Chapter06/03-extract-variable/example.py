d9d5f611493f5db2f25ff054dd70f9a97acb5f4a
Minwoo Jeong <jeongmwj@gmail.com>
2021-12-15 09:45:20 +0900

    Extract `quantity_discount` variable

--- Chapter06/03-extract-variable/example.py
+++ Chapter06/03-extract-variable/example.py
@@ -1,9 +1,7 @@
 def price(order):
     # price = base price - quantity discount + shipping
     base_price = order.quantity * order.item_price
-    return (
-        base_price
-        - max(0, order.quantity - 500) * order.item_price * 0.05
-        + min(base_price * 0.1, 100)
-    )
+    quantity_discount = max(0, order.quantity - 500) * order.item_price * 0.05
+
+    return base_price - quantity_discount + min(base_price * 0.1, 100)
 
