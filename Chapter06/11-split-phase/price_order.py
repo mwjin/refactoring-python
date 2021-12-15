dfae04b188ab38d4e714b5c1d680476eeb3f5c09
Minwoo Jeong <jeongmwj@gmail.com>
2021-12-15 20:29:03 +0900

    Replace the intermediate data with the transformer object

--- Chapter06/11-split-phase/price_order.py
+++ Chapter06/11-split-phase/price_order.py
@@ -2,21 +2,16 @@ from price_data import PriceData
 
 
 def price_order(product, quantity, shipping_method):
-    _price_data = PriceData(product, quantity)
-    price_data = {
-        "base price": _price_data.base_price,
-        "quantity": _price_data.quantity,
-        "discount": _price_data.discount,
-    }
+    price_data = PriceData(product, quantity)
     return apply_shipping(price_data, shipping_method)
 
 
 def apply_shipping(price_data, shipping_method):
     shipping_per_case = (
         shipping_method.discounted_fee
-        if price_data["base price"] > shipping_method.discount_threshold
+        if price_data.base_price > shipping_method.discount_threshold
         else shipping_method.fee_per_case
     )
-    shipping_cost = price_data["quantity"] * shipping_per_case
-    price = price_data["base price"] - price_data["discount"] + shipping_cost
+    shipping_cost = price_data.quantity * shipping_per_case
+    price = price_data.base_price - price_data.discount + shipping_cost
     return price
