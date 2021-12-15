2ef69b74a30be714ed2feed22d1a95eb0b4d56f5
Minwoo Jeong <jeongmwj@gmail.com>
2021-12-15 19:09:57 +0900

    Add a statement to append additional information to the `reading`

--- Chapter06/10-combine-functions-into-transform/client3.py
+++ Chapter06/10-combine-functions-into-transform/client3.py
@@ -1,10 +1,11 @@
 from dummy import base_rate
-from reading import acquire_reading
+from reading import acquire_reading, enrich_reading
 
 
 def calculate_base_charge(reading):
     return base_rate(reading["month"], reading["year"]) * reading["quantity"]
 
 
-reading = acquire_reading()
+raw_reading = acquire_reading()
+reading = enrich_reading(raw_reading)
 basic_charge_amount = calculate_base_charge(reading)
