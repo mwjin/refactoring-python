85086c463e35e57799ed69109b5526cddfa8d7dc
Minwoo Jeong <jeongmwj@gmail.com>
2021-12-15 17:30:29 +0900

    Encapsulate the reading record data

--- Chapter06/09-combine-functions-into-class/reading.py
+++ Chapter06/09-combine-functions-into-class/reading.py
@@ -1,5 +1,29 @@
 _reading = {"customer": "Minwoo", "quantity": 10, "month": 5, "year": 2017}
 
 
+class Reading:
+    def __init__(self, data):
+        self._customer = data["customer"]
+        self._quantity = data["quantity"]
+        self._month = data["month"]
+        self._year = data["year"]
+
+    @property
+    def customer(self):
+        return self._customer
+
+    @property
+    def quantity(self):
+        return self._quantity
+
+    @property
+    def month(self):
+        return self._month
+
+    @property
+    def year(self):
+        return self._year
+
+
 def acquire_reading():
     return dict(_reading)
