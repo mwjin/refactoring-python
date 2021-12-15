4c5c800ec6ce98af1ea2365cc9a81ab21dbc2381
Minwoo Jeong <jeongmwj@gmail.com>
2021-12-15 11:15:39 +0900

    Add a test for `in_new_england` function

--- Chapter06/05-change-function-declaration/tests.py
+++ Chapter06/05-change-function-declaration/tests.py
@@ -1,7 +1,10 @@
 import math
 
-from circum import circum
+from address import Address
 from book import Book
+from circum import circum
+from customer import Customer
+from in_new_england import in_new_england
 
 
 def test_circum():
@@ -12,3 +15,8 @@ def test_book_add_reservation():
     book = Book()
     book.add_reservation("Minwoo Jeong", False)
     assert "Minwoo Jeong" in book.reservations
+
+
+def test_in_new_england():
+    assert in_new_england(Customer(Address("MA")))
+    assert not in_new_england(Customer(Address("KO")))
