6823be2f605e6e10383788054bf969a9302fe25e
Minwoo Jeong <jeongmwj@gmail.com>
2021-12-15 09:49:38 +0900

    Add the test `test_order_price`

--- Chapter06/03-extract-variable/tests.py
+++ Chapter06/03-extract-variable/tests.py
@@ -6,3 +6,8 @@ from order import Order
 def test_price():
     assert price(Order(2000, 1000)) == 1925100
     assert price(Order(600, 100)) == 59600
+
+
+def test_order_price():
+    assert Order(2000, 1000).price == 1925100
+    assert Order(600, 100).price == 59600
