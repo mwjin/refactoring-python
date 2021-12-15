aead0c54976cac20502cc6404fa12d3307a1f308
Minwoo Jeong <jeongmwj@gmail.com>
2021-12-15 19:57:57 +0900

    Move `quantity` into the intermediate data

--- Chapter06/11-split-phase/price_order.py
+++ Chapter06/11-split-phase/price_order.py
@@ -5,17 +5,17 @@ def price_order(product, quantity, shipping_method):
         * product.base_price
         * product.discount_rate
     )
-    price_data = {"base price": base_price}
-    price = apply_shipping(price_data, quantity, shipping_method, discount)
+    price_data = {"base price": base_price, "quantity": quantity}
+    price = apply_shipping(price_data, shipping_method, discount)
     return price
 
 
-def apply_shipping(price_data, quantity, shipping_method, discount):
+def apply_shipping(price_data, shipping_method, discount):
     shipping_per_case = (
         shipping_method.discounted_fee
         if price_data["base price"] > shipping_method.discount_threshold
         else shipping_method.fee_per_case
     )
-    shipping_cost = quantity * shipping_per_case
+    shipping_cost = price_data["quantity"] * shipping_per_case
     price = price_data["base price"] - discount + shipping_cost
     return price
