35047781ecd5c6f6b8369f66a9c14ee31d21ee39
Minwoo Jeong <jeongmwj@gmail.com>
2021-12-15 13:46:45 +0900

    Add tests

---
+++ Chapter06/06-encapsulate-variable/tests.py
@@ -0,0 +1,15 @@
+from default_owner import default_owner
+
+
+def test_get_default_owner():
+    global default_owner
+    spaceship_owner = default_owner
+    assert spaceship_owner["first name"] == "Minwoo"
+    assert spaceship_owner["last name"] == "Jeong"
+
+
+def test_set_default_owner():
+    global default_owner
+    default_owner = {"first name": "Rebecca", "last name": "Parsons"}
+    assert default_owner["first name"] == "Rebecca"
+    assert default_owner["last name"]
