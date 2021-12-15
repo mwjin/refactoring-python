3d9c4c1ece6de1ee2d1937d498aa1f7614a0a4ac
Minwoo Jeong <jeongmwj@gmail.com>
2021-12-15 14:06:25 +0900

    Update \`default_owner\` to return the copy
    This is a procedure to encapsulate the field values

--- Chapter06/06-encapsulate-variable/default_owner.py
+++ Chapter06/06-encapsulate-variable/default_owner.py
@@ -2,7 +2,7 @@ _default_owner = {"first name": "Minwoo", "last name": "Jeong"}
 
 
 def default_owner():
-    return _default_owner
+    return dict(_default_owner)
 
 
 def set_default_owner(owner):
