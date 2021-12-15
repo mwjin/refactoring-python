ed950b70805722251534a4947a937e73998c140e
Minwoo Jeong <jeongmwj@gmail.com>
2021-12-15 09:43:37 +0900

    Extract `base_price` variable

--- Chapter06/03-extract-variable/example.py
+++ Chapter06/03-extract-variable/example.py
@@ -1,5 +1,6 @@
 def price(order):
     # price = base price - quantity discount + shipping
+    base_price = order.quantity * order.item_price
     return (
         order.quantity * order.item_price
         - max(0, order.quantity - 500) * order.item_price * 0.05
