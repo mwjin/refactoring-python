6d13c20287d4c9a025879cb32da7d364ad5abdef
Minwoo Jeong <jeongmwj@gmail.com>
2021-12-15 19:05:19 +0900

    Define a transform `enrich_reading`
    This function copies the input object deeply and returns this copy.

--- Chapter06/10-combine-functions-into-transform/reading.py
+++ Chapter06/10-combine-functions-into-transform/reading.py
@@ -1,5 +1,12 @@
+from copy import deepcopy
+
 _reading = {"customer": "Minwoo", "quantity": 10, "month": 5, "year": 2017}
 
 
 def acquire_reading():
     return dict(_reading)
+
+
+def enrich_reading(original):
+    result = deepcopy(original)
+    return result
