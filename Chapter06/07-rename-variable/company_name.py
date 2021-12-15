e77ef04966621b106fbbea896bbde0a0d518f5d9
Minwoo Jeong <jeongmwj@gmail.com>
2021-12-15 14:34:13 +0900

    Add another example with its tests

---
+++ Chapter06/07-rename-variable/company_name.py
@@ -0,0 +1,9 @@
+cpynm = "Samsung"
+
+
+def company_name():
+    return cpynm
+
+
+def print_company_name():
+    print(cpynm, end="")
--- Chapter06/07-rename-variable/tests.py
+++ Chapter06/07-rename-variable/tests.py
@@ -1,3 +1,4 @@
+import company_name
 import title
 
 
@@ -9,3 +10,13 @@ def test_read_title():
 def test_set_title():
     title.set_title("Hello")
     assert title.title() == "Hello"
+
+
+def test_company_name():
+    assert company_name.company_name() == "Samsung"
+
+
+def test_print_company_name(capsys):
+    company_name.print_company_name()
+    captured = capsys.readouterr()
+    assert captured.out == "Samsung"
