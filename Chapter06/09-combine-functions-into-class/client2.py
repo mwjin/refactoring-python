51d3b14b20b78942e9071889650d66751ef42724
Minwoo Jeong <jeongmwj@gmail.com>
2021-12-15 17:44:27 +0900

    Extract the `taxable_charge_fn` function

--- Chapter06/09-combine-functions-into-class/client2.py
+++ Chapter06/09-combine-functions-into-class/client2.py
@@ -1,7 +1,11 @@
 from dummy import base_rate, tax_threshold
 from reading import acquire_reading, Reading
 
+
+def taxable_charge_fn(reading):
+    return max(0, reading.base_charge - tax_threshold(reading.year))
+
+
 reading_data = acquire_reading()
 reading = Reading(reading_data)
-taxable_charge = max(0, reading.base_charge - tax_threshold(reading.year))
-
+taxable_charge = taxable_charge_fn(reading)
