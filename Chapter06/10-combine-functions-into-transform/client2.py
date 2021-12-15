b93b4dc47a036d0c963dd02f8ea36a95904f8391
Minwoo Jeong <jeongmwj@gmail.com>
2021-12-15 19:20:11 +0900

    Update the client2 to use the transformer

--- Chapter06/10-combine-functions-into-transform/client2.py
+++ Chapter06/10-combine-functions-into-transform/client2.py
@@ -1,6 +1,7 @@
-from dummy import base_rate, tax_threshold
-from reading import acquire_reading
+from dummy import tax_threshold
+from reading import acquire_reading, enrich_reading
 
-reading = acquire_reading()
-base = base_rate(reading["month"], reading["year"]) * reading["quantity"]
+raw_reading = acquire_reading()
+reading = enrich_reading(raw_reading)
+base = reading["base charge"]
 taxable_charge = max(0, base - tax_threshold(reading["year"]))
