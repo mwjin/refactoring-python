8fb606e1e6555f3827a32359166da7e2db979428
Minwoo Jeong <jeongmwj@gmail.com>
2021-12-15 10:23:02 +0900

    Define `Order` class

---
+++ Chapter06/04-inline-variable/order.py
@@ -0,0 +1,7 @@
+class Order:
+    def __init__(self, base_price):
+        self._base_price = base_price
+
+    def is_expensive(self):
+        base_price = self._base_price
+        return base_price > 1000
