16034e5ef247af531756f2f1006abdbf18f503e8
Minwoo Jeong <jeongmwj@gmail.com>
2021-12-15 11:19:26 +0900

    Extract the temporary function

--- Chapter06/05-change-function-declaration/in_new_england.py
+++ Chapter06/05-change-function-declaration/in_new_england.py
@@ -1,3 +1,7 @@
 def in_new_england(customer):
     state_code = customer.address.state
+    return xx_in_new_england(state_code)
+
+
+def xx_in_new_england(state_code):
     return state_code in ["MA", "CI", "ME", "VT", "NH", "RI"]
