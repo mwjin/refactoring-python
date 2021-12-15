b2b56d0faa65a0436d2dbe1aa564dfafa2612602
Minwoo Jeong <jeongmwj@gmail.com>
2021-12-15 11:24:17 +0900

    Inline the `in_new_england` function

--- Chapter06/05-change-function-declaration/in_new_england.py
+++ Chapter06/05-change-function-declaration/in_new_england.py
@@ -1,6 +1,2 @@
-def in_new_england(customer):
-    return xx_in_new_england(customer.address.state)
-
-
 def xx_in_new_england(state_code):
     return state_code in ["MA", "CI", "ME", "VT", "NH", "RI"]
