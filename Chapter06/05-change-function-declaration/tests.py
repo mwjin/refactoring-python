c3b02f5821a58fe79858ed91075469877a2ea2f3
Minwoo Jeong <jeongmwj@gmail.com>
2021-12-15 11:00:12 +0900

    Add a test for `Book` class

--- Chapter06/05-change-function-declaration/tests.py
+++ Chapter06/05-change-function-declaration/tests.py
@@ -1,7 +1,14 @@
 import math
 
 from circum import circum
+from book import Book
 
 
 def test_circum():
     assert circum(3) == 2 * math.pi * 3
+
+
+def test_book_add_reservation():
+    book = Book()
+    book.add_reservation("Minwoo Jeong")
+    assert "Minwoo Jeong" in book.reservations
