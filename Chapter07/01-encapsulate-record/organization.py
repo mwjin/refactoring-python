55e7cb6bd83944281a0ec572b2cb0d2128e1abad
Minwoo Jeong <jeongmwj@gmail.com>
2021-12-21 20:47:57 +0900

    Define `Organization` class
    in order to replace the record with the class
    and define 'get_organization' that returns the class instance.

--- Chapter07/01-encapsulate-record/organization.py
+++ Chapter07/01-encapsulate-record/organization.py
@@ -1,6 +1,16 @@
-organization = {"name": "Apple", "country": "KO"}
+class Organization:
+    def __init__(self, data) -> None:
+        self._data = data
+
+
+organization = Organization({"name": "Apple", "country": "KO"})
 
 
 def get_raw_data_of_organization():
+    global organization
+    return organization._data
+
+
+def get_organization():
     global organization
     return organization
