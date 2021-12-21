8cab00ea863312229199a007af50b77c17705746
Minwoo Jeong <jeongmwj@gmail.com>
2021-12-21 23:48:06 +0900

    Define methods to add and delete elements

--- Chapter07/02-encapsulate-collection/person.py
+++ Chapter07/02-encapsulate-collection/person.py
@@ -14,3 +14,10 @@ class Person:
     @courses.setter
     def courses(self, new_courses):
         self._courses = new_courses
+
+    def add_course(self, course):
+        self._courses.append(course)
+
+    def remove_course(self, course):
+        idx = self._courses.index(course)
+        del self._courses[idx]
