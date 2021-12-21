7e1bc8d006a83ddbb250cf0587772b951fce14c3
Minwoo Jeong <jeongmwj@gmail.com>
2021-12-21 21:46:19 +0900

    Define `CustomerData` class

--- Chapter07/01-encapsulate-record/customer_data.py
+++ Chapter07/01-encapsulate-record/customer_data.py
@@ -1,14 +1,23 @@
 import json
 
+
+class CustomerData:
+    def __init__(self, data) -> None:
+        self._data = data
+
+
 with open("customer_data.json") as infile:
-    customer_data = json.load(infile)
+    customer_data = CustomerData(json.load(infile))
+
+
+def get_customer_data():
+    return customer_data
 
 
 def get_raw_data_of_customers():
-    global customer_data
-    return customer_data
+    return customer_data._data
 
 
 def set_raw_data_of_customers(new_data):
     global customer_data
-    customer_data = new_data
+    customer_data = CustomerData(new_data)
