ee6902c2432de69726005b0a76ec7a82900a8ff7
Minwoo Jeong <jeongmwj@gmail.com>
2021-12-21 20:55:54 +0900

    Update the functions to use the getter and setter

--- Chapter07/01-encapsulate-record/example1.py
+++ Chapter07/01-encapsulate-record/example1.py
@@ -1,9 +1,9 @@
-from organization import get_raw_data_of_organization
+from organization import get_organization
 
 
 def get_org_name_as_header():
-    return f"<h1>{get_raw_data_of_organization()['name']}</h1>"
+    return f"<h1>{get_organization().name}</h1>"
 
 
 def set_org_name(new_name):
-    get_raw_data_of_organization()["name"] = new_name
+    get_organization().name = new_name
