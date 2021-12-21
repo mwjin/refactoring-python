d7b31724a875175b5939eb156094f403592fbb7c
Minwoo Jeong <jeongmwj@gmail.com>
2021-12-21 20:52:32 +0900

    Define a getter and a setter for the `name` field

--- Chapter07/01-encapsulate-record/organization.py
+++ Chapter07/01-encapsulate-record/organization.py
@@ -2,6 +2,14 @@ class Organization:
     def __init__(self, data) -> None:
         self._data = data
 
+    @property
+    def name(self):
+        return self._data["name"]
+
+    @name.setter
+    def name(self, new_name):
+        self._data["name"] = new_name
+
 
 organization = Organization({"name": "Apple", "country": "KO"})
 
