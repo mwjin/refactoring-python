4e8709f7d6670b62356a0286bfc3c84a18e5a5f5
Minwoo Jeong <jeongmwj@gmail.com>
2021-12-22 07:11:42 +0900

    Encapsulate the fields

--- Chapter07/03-replace-primitive-with-object/order.py
+++ Chapter07/03-replace-primitive-with-object/order.py
@@ -1,5 +1,20 @@
 class Order:
     def __init__(self, data) -> None:
-        self.priority = data["priority"]
-        self.id = data["id"]
+        self._priority = data["priority"]
+        self._id = data["id"]
 
+    @property
+    def priority(self):
+        return self._priority
+
+    @priority.setter
+    def priority(self, value):
+        self._priority = value
+
+    @property
+    def id(self):
+        return self._id
+
+    @id.setter
+    def id(self, value):
+        self._id = value
