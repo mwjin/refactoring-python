7e31a855d48e86226d0bcf65e69e779e5e4777cd
Minwoo Jeong <jeongmwj@gmail.com>
2021-12-21 21:02:27 +0900

    Unfold the `data` in the constructor
    and define a getter and a setter for the 'country'

--- Chapter07/01-encapsulate-record/organization.py
+++ Chapter07/01-encapsulate-record/organization.py
@@ -1,14 +1,23 @@
 class Organization:
     def __init__(self, data) -> None:
-        self._data = data
+        self._name = data["name"]
+        self._country = data["country"]
 
     @property
     def name(self):
-        return self._data["name"]
+        return self._name
 
     @name.setter
     def name(self, new_name):
-        self._data["name"] = new_name
+        self._name = new_name
+
+    @property
+    def country(self):
+        return self._country
+
+    @country.setter
+    def country(self, country_code):
+        self._country = country_code
 
 
 organization = Organization({"name": "Apple", "country": "KO"})
