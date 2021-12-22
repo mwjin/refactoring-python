bb91b8a8370601fda09638d4cead0b1b6d7198a6
Minwoo Jeong <jeongmwj@gmail.com>
2021-12-22 12:27:17 +0900

    Extract the getter of `base_price`

--- Chapter07/04-replace-temp-with-query/order.py
+++ Chapter07/04-replace-temp-with-query/order.py
@@ -5,13 +5,17 @@ class Order:
 
     @property
     def price(self):
-        base_price = self._quantity * self._item.price
+        base_price = self.base_price
         discount_factor = 0.98
 
         if base_price > 1000:
             discount_factor -= 0.03
 
         return base_price * discount_factor
+
+    @property
+    def base_price(self):
+        return self._quantity * self._item.price
 
 
 class Item:
