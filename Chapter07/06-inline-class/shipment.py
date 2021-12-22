bf2238fe73220f876fbda4f5f89311c487f3d43c
Minwoo Jeong <jeongmwj@gmail.com>
2021-12-22 14:18:26 +0900

    Remove `TrackingInformation` class

--- Chapter07/06-inline-class/shipment.py
+++ Chapter07/06-inline-class/shipment.py
@@ -30,29 +30,3 @@ class Shipment:
     @shipping_company.setter
     def shipping_company(self, shipping_company):
         self._shipping_company = shipping_company
-
-
-class TrackingInformation:
-    def __init__(self, shipping_company, tracking_number):
-        self._shipping_company = shipping_company
-        self._tracking_number = tracking_number
-
-    @property
-    def shipping_company(self):
-        return self._shipping_company
-
-    @shipping_company.setter
-    def shipping_company(self, shipping_company):
-        self._shipping_company = shipping_company
-
-    @property
-    def tracking_number(self):
-        return self._tracking_number
-
-    @tracking_number.setter
-    def tracking_number(self, tracking_number):
-        self._tracking_number = tracking_number
-
-    @property
-    def display(self):
-        return f"{self.shipping_company}: {self.tracking_number}"
