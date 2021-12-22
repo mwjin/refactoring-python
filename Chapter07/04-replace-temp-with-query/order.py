30f20fe8e95141f7f3d20b7bdb282a04ed697341
Minwoo Jeong <jeongmwj@gmail.com>
2021-12-22 12:29:53 +0900

    Extract the getter of `discount_factor`

--- Chapter07/04-replace-temp-with-query/order.py
+++ Chapter07/04-replace-temp-with-query/order.py
@@ -5,16 +5,19 @@ class Order:
 
     @property
     def price(self):
-        discount_factor = 0.98
-
-        if self.base_price > 1000:
-            discount_factor -= 0.03
-
+        discount_factor = self.discount_factor
         return self.base_price * discount_factor
 
     @property
     def base_price(self):
         return self._quantity * self._item.price
+
+    @property
+    def discount_factor(self):
+        discount_factor = 0.98
+        if self.base_price > 1000:
+            discount_factor -= 0.03
+        return discount_factor
 
 
 class Item:
