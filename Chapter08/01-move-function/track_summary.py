b3af89aaa86086cffe73a27eb49b3f8fe3b99e3f
Minwoo Jeong <jeongmwj@gmail.com>
2021-12-27 23:26:13 +0900

    Update the `calculate_distance` to call
    the 'top_calculate_distance' function

--- Chapter08/01-move-function/track_summary.py
+++ Chapter08/01-move-function/track_summary.py
@@ -6,26 +6,7 @@ def track_summary(points):
         return 1000000
 
     def calculate_distance():
-        def distance(p1, p2):
-            # haversine formula
-            EARTH_RADIUS = 3959
-            d_lat = radians(p2.lat) - radians(p1.lat)
-            d_lon = radians(p2.lon) - radians(p1.lon)
-            a = (
-                math.pow(math.sin(d_lat / 2), 2)
-                + math.cos(radians(p2.lat))
-                + math.cos(radians(p1.lat)) * math.pow(math.sin(d_lon / 2), 2)
-            )
-            c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
-            return EARTH_RADIUS * c
-
-        def radians(degrees):
-            return degrees * math.pi / 100
-
-        result = 0
-        for i in range(1, len(points)):
-            result += distance(points[i - 1], points[i])
-        return result
+        return top_calculate_distance(points)
 
     total_time = calculate_time()
     total_distance = round(calculate_distance(), 3)
