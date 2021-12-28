431d2789327cda1fbaeae5485c7131cadabddb36
Minwoo Jeong <jeongmwj@gmail.com>
2021-12-28 23:25:57 +0900

    Initialize `Account` and `AccountType`

---
+++ Chapter08/02-move-field/account.py
@@ -0,0 +1,14 @@
+class Account:
+    def __init__(self, number, type, interest_rate) -> None:
+        self._number = number
+        self._type = type
+        self._interest_rate = interest_rate
+
+    @property
+    def interest_rate(self):
+        return self._interest_rate
+
+
+class AccountType:
+    def __init__(self, name) -> None:
+        self._name = name
