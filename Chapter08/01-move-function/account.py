3eb1937efc123a5ca81e850661443f9186db1d13
Minwoo Jeong <jeongmwj@gmail.com>
2021-12-28 22:14:54 +0900

    Add classes and the test for the example 2
    - Add 'Account' and 'AccountType' classes
    - Add a test for the bank_charge property

---
+++ Chapter08/01-move-function/account.py
@@ -0,0 +1,39 @@
+class Account:
+    def __init__(self, days_overdrawn, type) -> None:
+        self._days_overdrawn = days_overdrawn
+        self._type = type
+
+    @property
+    def days_overdrawn(self):
+        return self._days_overdrawn
+
+    @property
+    def type(self):
+        return self._type
+
+    @property
+    def bank_charge(self):
+        result = 4.5
+        if self._days_overdrawn > 0:
+            result += self.over_draft_charge
+        return result
+
+    @property
+    def over_draft_charge(self):
+        if self.type.is_premium:
+            base_charge = 10
+            if self.days_overdrawn <= 7:
+                return base_charge
+            else:
+                return base_charge + (self.days_overdrawn - 7) * 0.05
+        else:
+            return self.days_overdrawn * 1.75
+
+
+class AccountType:
+    def __init__(self, is_premium):
+        self._is_premium = is_premium
+
+    @property
+    def is_premium(self):
+        return self._is_premium
--- Chapter08/01-move-function/tests.py
+++ Chapter08/01-move-function/tests.py
@@ -1,3 +1,4 @@
+from account import Account, AccountType
 from point import Point
 from track_summary import track_summary
 
@@ -12,3 +13,10 @@ def test_track_summary():
         "pace": 3.779,
         "time": 1000000,
     }
+
+
+def test_account_bank_charge():
+    assert Account(5, AccountType(True)).bank_charge == 14.5
+    assert Account(5, AccountType(False)).bank_charge == 13.25
+    assert Account(10, AccountType(True)).bank_charge == 14.65
+    assert Account(10, AccountType(False)).bank_charge == 22.0
