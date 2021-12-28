9668a05608de15c7dfa87831156832e88c177aa6
Minwoo Jeong <jeongmwj@gmail.com>
2021-12-28 23:13:29 +0900

    Encapsulate the `discount_rate` field

--- Chapter08/02-move-field/customer.py
+++ Chapter08/02-move-field/customer.py
@@ -11,12 +11,15 @@ class Customer:
     def discount_rate(self):
         return self._discount_rate
 
+    def _set_discount_rate(self, discount_rate):
+        self._discount_rate = discount_rate
+
     def become_preferred(self):
-        self._discount_rate += 0.03
+        self._set_discount_rate(self.discount_rate + 0.03)
         # Other good things...
 
     def apply_discount(self, amount):
-        return amount - amount * self._discount_rate
+        return amount - amount * self.discount_rate
 
 
 class CustomerContract:
