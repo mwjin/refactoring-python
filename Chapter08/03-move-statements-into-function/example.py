8ca188e8399e7ae2e7fda27de97843e765ec22ea
Minwoo Jeong <jeongmwj@gmail.com>
2022-01-02 22:32:52 +0900

    Initialize the example source code

---
+++ Chapter08/03-move-statements-into-function/example.py
@@ -0,0 +1,29 @@
+def render_person(person):
+    result = []
+    result.append(f"<p>{person.name}</p>")
+    result.append(render_photo(person.photo))
+    result.append(f"<p>Title: {person.photo.title}</p>")
+    result.append(emit_photo_data(person.photo))
+    return "\n".join(result)
+
+
+def render_photo(photo):
+    return f"<p>(Rendered HTML of the photo '{photo.title}')</p>"
+
+
+def photo_div(photo):
+    return "\n".join(
+        [
+            "<div>",
+            f"<p>Title: {photo.title}</p>",
+            emit_photo_data(photo),
+            "</div>",
+        ]
+    )
+
+
+def emit_photo_data(photo):
+    result = []
+    result.append(f"<p>Location: {photo.location}</p>")
+    result.append(f"<p>Date: {photo.date}</p>")
+    return "\n".join(result)
