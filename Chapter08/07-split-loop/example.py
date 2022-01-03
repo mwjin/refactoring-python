c2f1035ffaf64ca9ee54ac360fb0090a0aa32323
Minwoo Jeong <jeongmwj@gmail.com>
2022-01-03 21:42:35 +0900

    Extract a function for each loop

--- Chapter08/07-split-loop/example.py
+++ Chapter08/07-split-loop/example.py
@@ -1,12 +1,20 @@
 def get_youngest_and_total_salary(people):
+    return (
+        f"Youngest: {get_youngest_age(people)}, "
+        f"Total Salary: {get_total_salary(people)}"
+    )
+
+
+def get_youngest_age(people):
     youngest = people[0].age
     for person in people:
         if person.age < youngest:
             youngest = person.age
+    return youngest
 
+
+def get_total_salary(people):
     total_salary = 0
     for person in people:
         total_salary += person.salary
-
-    return f"Youngest: {youngest}, Total Salary: {total_salary}"
-
+    return total_salary
