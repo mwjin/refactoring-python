c38b446596a7492cf259eadc90e9d4979864c87b
Minwoo Jeong <jeongmwj@gmail.com>
2022-01-03 23:36:21 +0900

    Remove the for loop and generate the result from the pipeline

--- Chapter08/08-replace-loop-with-pipeline/example.py
+++ Chapter08/08-replace-loop-with-pipeline/example.py
@@ -1,18 +1,19 @@
 def acquire_data(input):
     lines = input.split("\n")
-    result = []
-    loop_items = map(
-        lambda record: {"city": record[0].strip(), "phone": record[2].strip()},
-        filter(
-            lambda record: record[1].strip() == "India",
-            map(
-                lambda line: line.split(","),
-                filter(lambda line: line.strip() != "", lines[1:]),
+    result = list(
+        map(
+            lambda record: {
+                "city": record[0].strip(),
+                "phone": record[2].strip(),
+            },
+            filter(
+                lambda record: record[1].strip() == "India",
+                map(
+                    lambda line: line.split(","),
+                    filter(lambda line: line.strip() != "", lines[1:]),
+                ),
             ),
-        ),
+        )
     )
-    for line in loop_items:
-        record = line
-        result.append(line)
 
     return result
