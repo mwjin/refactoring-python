ac600f6ca826260dfc361b04f4504ce78e9ee4b6
Minwoo Jeong <jeongmwj@gmail.com>
2022-01-03 23:27:40 +0900

    Use `map` to split the line

--- Chapter08/08-replace-loop-with-pipeline/example.py
+++ Chapter08/08-replace-loop-with-pipeline/example.py
@@ -1,9 +1,12 @@
 def acquire_data(input):
     lines = input.split("\n")
     result = []
-    loop_items = filter(lambda line: line.strip() != "", lines[1:])
+    loop_items = map(
+        lambda line: line.split(","),
+        filter(lambda line: line.strip() != "", lines[1:]),
+    )
     for line in loop_items:
-        record = line.split(",")
+        record = line
         if record[1].strip() == "India":
             result.append(
                 {"city": record[0].strip(), "phone": record[2].strip()}
