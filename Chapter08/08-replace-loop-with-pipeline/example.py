522c9ac6501f3ab25eb11083e8ba6ec5182e6a66
Minwoo Jeong <jeongmwj@gmail.com>
2022-01-03 23:33:52 +0900

    Use `map` to generate the result records

--- Chapter08/08-replace-loop-with-pipeline/example.py
+++ Chapter08/08-replace-loop-with-pipeline/example.py
@@ -1,15 +1,18 @@
 def acquire_data(input):
     lines = input.split("\n")
     result = []
-    loop_items = filter(
-        lambda record: record[1].strip() == "India",
-        map(
-            lambda line: line.split(","),
-            filter(lambda line: line.strip() != "", lines[1:]),
+    loop_items = map(
+        lambda record: {"city": record[0].strip(), "phone": record[2].strip()},
+        filter(
+            lambda record: record[1].strip() == "India",
+            map(
+                lambda line: line.split(","),
+                filter(lambda line: line.strip() != "", lines[1:]),
+            ),
         ),
     )
     for line in loop_items:
         record = line
-        result.append({"city": record[0].strip(), "phone": record[2].strip()})
+        result.append(line)
 
     return result
