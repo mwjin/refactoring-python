d2453f484102a580e1859b9cc81c615c3b6bc1cb
Minwoo Jeong <jeongmwj@gmail.com>
2022-01-05 07:01:38 +0900

    Encapsulate the `organization` record

--- Chapter09/02-rename-field/example.py
+++ Chapter09/02-rename-field/example.py
@@ -2,4 +2,4 @@ from organization import organization
 
 
 def get_organization_info():
-    return f"{organization['name']} ({organization['country']})"
+    return f"{organization.name} ({organization.country})"
--- Chapter09/02-rename-field/organization.py
+++ Chapter09/02-rename-field/organization.py
@@ -1,1 +1,23 @@
-organization = {"name": "Samsung", "country": "KO"}
+class Organization:
+    def __init__(self, data):
+        self._name = data["name"]
+        self._country = data["country"]
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, new_name):
+        self._name = new_name
+
+    @property
+    def country(self):
+        return self._country
+
+    @country.setter
+    def country(self, new_country):
+        self._country = new_country
+
+
+organization = Organization({"name": "Samsung", "country": "KO"})
