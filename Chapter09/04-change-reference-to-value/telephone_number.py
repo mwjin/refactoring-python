07ca57a09b0584b6fbf855dc22cdd89594f2d346
Minwoo Jeong <jeongmwj@gmail.com>
2022-01-05 11:22:39 +0900

    Remove the setters
    'TypeError' would occur during testing

--- Chapter09/04-change-reference-to-value/telephone_number.py
+++ Chapter09/04-change-reference-to-value/telephone_number.py
@@ -1,20 +1,12 @@
 class TelephoneNumber:
-    def __init__(self):
-        self._area_code = 82
-        self._number = 123456789
+    def __init__(self, area_code, number):
+        self._area_code = area_code
+        self._number = number
 
     @property
     def area_code(self):
         return self._area_code
 
-    @area_code.setter
-    def area_code(self, arg):
-        self._area_code = arg
-
     @property
     def number(self):
         return self._number
-
-    @number.setter
-    def number(self, arg):
-        self._number = arg
