69a0c6eb08ee18181aa9b0fc47b71c1b948893fc
Minwoo Jeong <jeongmwj@gmail.com>
2022-01-05 10:51:59 +0900

    Split the `_production` variable

--- Chapter09/03-replace-derived-variable-with-query/production_plan2.py
+++ Chapter09/03-replace-derived-variable-with-query/production_plan2.py
@@ -3,13 +3,14 @@ from functools import reduce
 
 class ProductionPlan:
     def __init__(self, production):
-        self._production = production
+        self._initial_production = production
+        self._production_accumulator = 0
         self._adjustments = []
 
     @property
     def production(self):
-        return self._production
+        return self._initial_production + self._production_accumulator
 
     def apply_adjustment(self, adjustment):
         self._adjustments.append(adjustment)
-        self._production += adjustment.amount
+        self._production_accumulator += adjustment.amount
