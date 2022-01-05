c2ed6586ecfe4dfdd9a97faa850f539f20c3a2a3
Minwoo Jeong <jeongmwj@gmail.com>
2022-01-05 10:55:03 +0900

    Remove the dead code

--- Chapter09/03-replace-derived-variable-with-query/production_plan2.py
+++ Chapter09/03-replace-derived-variable-with-query/production_plan2.py
@@ -4,7 +4,6 @@ from functools import reduce
 class ProductionPlan:
     def __init__(self, production):
         self._initial_production = production
-        self._production_accumulator = 0
         self._adjustments = []
 
     @property
@@ -17,4 +16,3 @@ class ProductionPlan:
 
     def apply_adjustment(self, adjustment):
         self._adjustments.append(adjustment)
-        self._production_accumulator += adjustment.amount
