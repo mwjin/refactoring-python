3bb3c5e9bb056fcbb7bc6cf34a6ce7e30f202b9e
Minwoo Jeong <jeongmwj@gmail.com>
2022-01-05 10:42:04 +0900

    Add `calculated_production` property and validate it

--- Chapter09/03-replace-derived-variable-with-query/production_plan.py
+++ Chapter09/03-replace-derived-variable-with-query/production_plan.py
@@ -1,3 +1,6 @@
+from functools import reduce
+
+
 class ProductionPlan:
     def __init__(self):
         self._production = 0
@@ -5,7 +8,12 @@ class ProductionPlan:
 
     @property
     def production(self):
+        assert self._production == self.calculated_production
         return self._production
+
+    @property
+    def calculated_production(self):
+        return reduce(lambda sum, a: sum + a.amount, self._adjustments, 0)
 
     def apply_adjustment(self, adjustment):
         self._adjustments.append(adjustment)
