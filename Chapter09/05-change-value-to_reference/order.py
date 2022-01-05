31f029bd55ee80657552d72d41883af9093fc80a
Minwoo Jeong <jeongmwj@gmail.com>
2022-01-05 12:46:42 +0900

    Get the `Customer` instance from the repository

--- Chapter09/05-change-value-to_reference/order.py
+++ Chapter09/05-change-value-to_reference/order.py
@@ -4,7 +4,7 @@ from customer import Customer
 class Order:
     def __init__(self, data):
         self._number = data["number"]
-        self._customer = Customer(data["customer"])
+        self._customer = Customer.register_customer(data["customer"])
 
     @property
     def customer(self):
