bae81faf893f125121c9f44b68db88581037d849
Minwoo Jeong <jeongmwj@gmail.com>
2022-01-07 23:35:05 +0900

    Extract `is_summer` function

--- Chapter10/01-decompose-conditional/example.py
+++ Chapter10/01-decompose-conditional/example.py
@@ -1,6 +1,10 @@
 def get_charge(quantity, date, plan):
-    if not date < plan.summer_start and not date > plan.summer_end:
+    if is_summer(date, plan):
         charge = quantity * plan.summer_rate
     else:
         charge = quantity * plan.regular_rate + plan.regular_service_charge
     return charge
+
+
+def is_summer(date, plan):
+    return not date < plan.summer_start and not date > plan.summer_end
