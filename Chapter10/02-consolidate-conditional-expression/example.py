6094ace325a9fa8c93b87caea1eb1a325ca95919
Minwoo Jeong <jeongmwj@gmail.com>
2022-01-10 20:01:19 +0900

    Extract `is_not_eligible_for_disability` function

--- Chapter10/02-consolidate-conditional-expression/example.py
+++ Chapter10/02-consolidate-conditional-expression/example.py
@@ -1,8 +1,12 @@
 def disability_amount(employee):
-    if (
+    if is_not_eligible_for_disability(employee):
+        return 0
+    return 1
+
+
+def is_not_eligible_for_disability(employee):
+    return (
         employee.seniority < 2
         or employee.months_disabled > 12
         or employee.is_part_time
-    ):
-        return 0
-    return 1
+    )
