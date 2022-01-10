e0a6eb0c73d36d845a016cdb34b6169159391d24
Minwoo Jeong <jeongmwj@gmail.com>
2022-01-11 06:45:49 +0900

    Remove the remain conditional with a guard clause

--- Chapter10/03-replace-nested-conditional-with-guard-clauses/example.py
+++ Chapter10/03-replace-nested-conditional-with-guard-clauses/example.py
@@ -2,8 +2,7 @@ def pay_amount(employee):
     if employee.is_separated:
         return {"amount": 0, "reasonCode": "SEP"}
     if employee.is_retired:
-        result = {"amount": 0, "reasonCode": "RET"}
-    else:
-        # Something to calculate pay
-        result = {"amount": 10000, "reasonCode": "None"}
+        return {"amount": 0, "reasonCode": "RET"}
+    # Something to calculate pay
+    result = {"amount": 10000, "reasonCode": "None"}
     return result
