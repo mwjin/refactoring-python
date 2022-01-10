06e7d92bd38b34694021ca20a3928ec154f37868
Minwoo Jeong <jeongmwj@gmail.com>
2022-01-10 19:59:52 +0900

    Consolidate all the conditional expressions

--- Chapter10/02-consolidate-conditional-expression/example.py
+++ Chapter10/02-consolidate-conditional-expression/example.py
@@ -1,6 +1,8 @@
 def disability_amount(employee):
-    if employee.seniority < 2 or employee.months_disabled > 12:
-        return 0
-    if employee.is_part_time:
+    if (
+        employee.seniority < 2
+        or employee.months_disabled > 12
+        or employee.is_part_time
+    ):
         return 0
     return 1
