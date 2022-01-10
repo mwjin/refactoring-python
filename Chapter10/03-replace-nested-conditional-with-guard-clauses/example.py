a9d559dc88d9bf143a52fa0834cdd1420f90bd59
Minwoo Jeong <jeongmwj@gmail.com>
2022-01-11 06:44:34 +0900

    Replace the top level conditional expression with a guard clause

--- Chapter10/03-replace-nested-conditional-with-guard-clauses/example.py
+++ Chapter10/03-replace-nested-conditional-with-guard-clauses/example.py
@@ -1,10 +1,9 @@
 def pay_amount(employee):
     if employee.is_separated:
-        result = {"amount": 0, "reasonCode": "SEP"}
+        return {"amount": 0, "reasonCode": "SEP"}
+    if employee.is_retired:
+        result = {"amount": 0, "reasonCode": "RET"}
     else:
-        if employee.is_retired:
-            result = {"amount": 0, "reasonCode": "RET"}
-        else:
-            # Something to calculate pay
-            result = {"amount": 10000, "reasonCode": "None"}
+        # Something to calculate pay
+        result = {"amount": 10000, "reasonCode": "None"}
     return result
