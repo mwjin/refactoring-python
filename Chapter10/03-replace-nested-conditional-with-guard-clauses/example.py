0feb453f2ff7cbf15ab3040bc14c4052bed4ff1f
Minwoo Jeong <jeongmwj@gmail.com>
2022-01-11 06:46:26 +0900

    Remove the `result` variable

--- Chapter10/03-replace-nested-conditional-with-guard-clauses/example.py
+++ Chapter10/03-replace-nested-conditional-with-guard-clauses/example.py
@@ -4,5 +4,4 @@ def pay_amount(employee):
     if employee.is_retired:
         return {"amount": 0, "reasonCode": "RET"}
     # Something to calculate pay
-    result = {"amount": 10000, "reasonCode": "None"}
-    return result
+    return {"amount": 10000, "reasonCode": "None"}
