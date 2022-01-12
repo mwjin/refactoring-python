2aad14eeed3aebdafe5df51ee102bb605a2b6a25
Minwoo Jeong <jeongmwj@gmail.com>
2022-01-12 14:38:52 +0900

    Combine the two function into a `Bird` class

--- Chapter10/04-replace-conditional-with-polymorphism/bird.py
+++ Chapter10/04-replace-conditional-with-polymorphism/bird.py
@@ -1,3 +1,34 @@
+class Bird:
+    def __init__(self, bird_object) -> None:
+        self._name = bird_object.name
+        self._type = bird_object.type
+        self._number_of_coconuts = bird_object.number_of_coconuts
+        self._voltage = bird_object.voltage
+        self._is_nailed = bird_object.is_nailed
+
+    @property
+    def plumage(self):
+        if self._type == "European Swallow":
+            return "Normal"
+        elif self._type == "African Swallow":
+            return "Exhausted" if self._number_of_coconuts > 2 else "Normal"
+        elif self._type == "Norwegian Blue Parrot":
+            return "Scorched" if self._voltage > 100 else "Pretty"
+        else:
+            return "Unknown"
+
+    @property
+    def air_speed_velocity(self):
+        if self._type == "European Swallow":
+            return 35
+        elif self._type == "African Swallow":
+            return 40 - 2 * self._number_of_coconuts
+        elif self._type == "Norwegian Blue Parrot":
+            return 0 if self._is_nailed else 10 + self._voltage / 10
+        else:
+            return None
+
+
 class BirdObject:
     def __init__(self, name, type):
         self.name = name
@@ -16,22 +47,8 @@ def speeds(birds):
 
 
 def plumage(bird):
-    if bird.type == "European Swallow":
-        return "Normal"
-    elif bird.type == "African Swallow":
-        return "Exhausted" if bird.number_of_coconuts > 2 else "Normal"
-    elif bird.type == "Norwegian Blue Parrot":
-        return "Scorched" if bird.voltage > 100 else "Pretty"
-    else:
-        return "Unknown"
+    return Bird(bird).plumage
 
 
 def air_speed_velocity(bird):
-    if bird.type == "European Swallow":
-        return 35
-    elif bird.type == "African Swallow":
-        return 40 - 2 * bird.number_of_coconuts
-    elif bird.type == "Norwegian Blue Parrot":
-        return 0 if bird.is_nailed else 10 + bird.voltage / 10
-    else:
-        return None
+    return Bird(bird).air_speed_velocity
