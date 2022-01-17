af1332ce81ada805cf7b8174fb81f34d5ef82276
Minwoo Jeong <jeongmwj@gmail.com>
2022-01-17 12:39:53 +0900

    Update the `customer` property to return `UnknownCustomer` instance

--- Chapter10/05-introduce-special-case/my_site.py
+++ Chapter10/05-introduce-special-case/my_site.py
@@ -1,7 +1,14 @@
+from customer import UnknownCustomer
+
+
 class Site:
     def __init__(self, customer):
         self._customer = customer
 
     @property
     def customer(self):
-        return self._customer
+        return (
+            UnknownCustomer()
+            if self._customer == "Unknown Customer"
+            else self._customer
+        )
