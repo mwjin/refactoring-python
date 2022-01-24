b5babaa6e4ddf2d593d30f9fba8b148a4ead43a1
Minwoo Jeong <jeongmwj@gmail.com>
2022-01-24 12:33:58 +0900

    Duplicate the function and rename the duplicate

--- Chapter11/01-seperate-query-from-modifier/example.py
+++ Chapter11/01-seperate-query-from-modifier/example.py
@@ -9,5 +9,16 @@ def alert_for_miscreant(people):
     return ""
 
 
+def find_miscreant(people):
+    for p in people:
+        if p == "Joker":
+            set_off_alarms()
+            return "Joker"
+        if p == "Saruman":
+            set_off_alarms()
+            return "Saruman"
+    return ""
+
+
 def set_off_alarms():
     print("A miscreant has been detected!")
