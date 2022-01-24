b96e5258d3185dbc32d5b7414b6aecf6bf46b156
Minwoo Jeong <jeongmwj@gmail.com>
2022-01-24 13:08:26 +0900

    Replace the `top_band` with the `within_band`

--- Chapter11/02-parameterize-function/example.py
+++ Chapter11/02-parameterize-function/example.py
@@ -1,10 +1,13 @@
+from math import inf
+
+
 def base_charge(usage):
     if usage < 0:
         return usd(0)
     amount = (
         within_band(usage, 0, 100) * 0.03
         + within_band(usage, 100, 200) * 0.05
-        + top_band(usage) * 0.07
+        + within_band(usage, 200, inf) * 0.07
     )
     return usd(amount)
 
@@ -15,7 +18,3 @@ def usd(amount):
 
 def within_band(usage, bottom, top):
     return min(usage, top) - bottom if usage > bottom else 0
-
-
-def top_band(usage):
-    return usage - 200 if usage > 200 else 0
