343f2563b6ecc2748d423fae42c1c8d36b20b0ee
Minwoo Jeong <jeongmwj@gmail.com>
2022-01-24 13:04:49 +0900

    Replace the literal with the parameter `bottom`

--- Chapter11/02-parameterize-function/example.py
+++ Chapter11/02-parameterize-function/example.py
@@ -18,7 +18,7 @@ def bottom_band(usage):
 
 
 def within_band(usage, bottom, top):
-    return min(usage, 200) - 100 if usage > 100 else 0
+    return min(usage, 200) - bottom if usage > bottom else 0
 
 
 def top_band(usage):
