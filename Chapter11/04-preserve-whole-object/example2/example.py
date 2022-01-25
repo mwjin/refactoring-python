7ee493f33ad4b0e427994063219bdd2b26f18985
Minwoo Jeong <jeongmwj@gmail.com>
2022-01-25 20:14:47 +0900

    Rename `new_within_range` to `within_range`

--- Chapter11/04-preserve-whole-object/example2/example.py
+++ Chapter11/04-preserve-whole-object/example2/example.py
@@ -1,6 +1,6 @@
 def check_room_temperature(plan, room):
     temp_range = room.days_temp_range
-    is_within_range = plan.new_within_range(temp_range)
+    is_within_range = plan.within_range(temp_range)
     if not is_within_range:
         return "The room temperature is out of the range."
     return "The room temperature is in appropriate range."
--- Chapter11/04-preserve-whole-object/example2/heating_plan.py
+++ Chapter11/04-preserve-whole-object/example2/heating_plan.py
@@ -2,7 +2,7 @@ class HeatingPlan:
     def __init__(self, temperature_range) -> None:
         self._temperature_range = temperature_range
 
-    def new_within_range(self, temp_range):
+    def within_range(self, temp_range):
         return (
             temp_range.low >= self._temperature_range.low
             and temp_range.high <= self._temperature_range.high
