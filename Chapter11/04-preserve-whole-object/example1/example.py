b4660c1907654a3e9ad93cace813cadf77def6c9
Minwoo Jeong <jeongmwj@gmail.com>
2022-01-25 19:59:35 +0900

    Remove the dead code

--- Chapter11/04-preserve-whole-object/example1/example.py
+++ Chapter11/04-preserve-whole-object/example1/example.py
@@ -1,6 +1,4 @@
 def check_room_temperature(plan, room):
-    low = room.days_temp_range.low
-    high = room.days_temp_range.high
     if not plan.new_within_range(room.days_temp_range):
         return "The room temperature is out of the range."
     return "The room temperature is in appropriate range."
