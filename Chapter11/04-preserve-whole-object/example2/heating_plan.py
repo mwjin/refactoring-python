5247874a946b850dbea2f4e248b69c6f0e262dfa
Minwoo Jeong <jeongmwj@gmail.com>
2022-01-25 20:14:03 +0900

    Inline `within_range` method

--- Chapter11/04-preserve-whole-object/example2/heating_plan.py
+++ Chapter11/04-preserve-whole-object/example2/heating_plan.py
@@ -2,11 +2,8 @@ class HeatingPlan:
     def __init__(self, temperature_range) -> None:
         self._temperature_range = temperature_range
 
-    def within_range(self, bottom, top):
-        return (
-            bottom >= self._temperature_range.low
-            and top <= self._temperature_range.high
-        )
-
     def new_within_range(self, temp_range):
-        return self.within_range(temp_range.low, temp_range.high)
+        return (
+            temp_range.low >= self._temperature_range.low
+            and temp_range.high <= self._temperature_range.high
+        )
