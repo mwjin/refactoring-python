33429b066d2da1cbae54c0beca64fa930beb8ad5
Minwoo Jeong <jeongmwj@gmail.com>
2022-01-25 20:07:02 +0900

    Extract `is_within_range` variable

--- Chapter11/04-preserve-whole-object/example2/example.py
+++ Chapter11/04-preserve-whole-object/example2/example.py
@@ -1,6 +1,7 @@
 def check_room_temperature(plan, room):
     low = room.days_temp_range.low
     high = room.days_temp_range.high
-    if not plan.within_range(low, high):
+    is_within_range = plan.within_range(low, high)
+    if not is_within_range:
         return "The room temperature is out of the range."
     return "The room temperature is in appropriate range."
