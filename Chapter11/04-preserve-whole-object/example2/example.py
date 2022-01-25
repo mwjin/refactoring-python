a9f834e81d280d7c544041a0ec173ce729b0a127
Minwoo Jeong <jeongmwj@gmail.com>
2022-01-25 20:08:11 +0900

    Extract `temp_range` variable

--- Chapter11/04-preserve-whole-object/example2/example.py
+++ Chapter11/04-preserve-whole-object/example2/example.py
@@ -1,6 +1,7 @@
 def check_room_temperature(plan, room):
-    low = room.days_temp_range.low
-    high = room.days_temp_range.high
+    temp_range = room.days_temp_range
+    low = temp_range.low
+    high = temp_range.high
     is_within_range = plan.within_range(low, high)
     if not is_within_range:
         return "The room temperature is out of the range."
