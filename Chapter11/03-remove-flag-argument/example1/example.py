f4de2fb73ef83cc2a0ef529ccbf36d9aa5a8236f
Minwoo Jeong <jeongmwj@gmail.com>
2022-01-25 19:16:02 +0900

    Decompose the conditional expression

--- Chapter11/03-remove-flag-argument/example1/example.py
+++ Chapter11/03-remove-flag-argument/example1/example.py
@@ -1,17 +1,25 @@
 def delivery_date(order, is_rush):
     if is_rush:
-        if order.delivery_state in ["MA", "CT"]:
-            delivery_time = 1
-        elif order.delivery_state in ["NY", "NH"]:
-            delivery_time = 2
-        else:
-            delivery_time = 3
-        return order.add_days_to_placed_on(1 + delivery_time)
+        return rush_delivery_date(order)
     else:
-        if order.delivery_state in ["MA", "CT", "NY"]:
-            delivery_time = 2
-        elif order.delivery_state in ["ME", "NH"]:
-            delivery_time = 3
-        else:
-            delivery_time = 4
-        return order.add_days_to_placed_on(2 + delivery_time)
+        return regular_delivery_date(order)
+
+
+def rush_delivery_date(order):
+    if order.delivery_state in ["MA", "CT"]:
+        delivery_time = 1
+    elif order.delivery_state in ["NY", "NH"]:
+        delivery_time = 2
+    else:
+        delivery_time = 3
+    return order.add_days_to_placed_on(1 + delivery_time)
+
+
+def regular_delivery_date(order):
+    if order.delivery_state in ["MA", "CT", "NY"]:
+        delivery_time = 2
+    elif order.delivery_state in ["ME", "NH"]:
+        delivery_time = 3
+    else:
+        delivery_time = 4
+    return order.add_days_to_placed_on(2 + delivery_time)
