b136690dde56d39f58a6a0e7f46754a843c8edb3
Minwoo Jeong <jeongmwj@gmail.com>
2022-01-26 16:22:50 +0900

    Update the test to create the instance using the new constructor

--- Chapter11/07-remove-setting-method/test_person.py
+++ Chapter11/07-remove-setting-method/test_person.py
@@ -3,8 +3,7 @@ from person import Person
 
 def test_person_properties():
     """This test is somewhat redundant but exists for the example."""
-    person = Person()
-    person.id = 1234
+    person = Person(1234)
     person.name = "Minwoo Jeong"
 
     assert person.id == 1234
