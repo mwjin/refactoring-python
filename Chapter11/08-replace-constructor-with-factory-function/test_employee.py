244bd602187c89490dcb4c120dcd26e43eef990e
Minwoo Jeong <jeongmwj@gmail.com>
2022-01-27 12:23:09 +0900

    Replace the factory function with the new ones
    because it is a code smell to use a literal as an argument

--- Chapter11/08-replace-constructor-with-factory-function/test_employee.py
+++ Chapter11/08-replace-constructor-with-factory-function/test_employee.py
@@ -1,7 +1,7 @@
-from employee import create_employee
+from employee import create_engineer, create_manager, create_salesperson
 
 
 def test_employee_type():
-    assert create_employee("Minwoo Jeong", "E").type == "Engineer"
-    assert create_employee("Minwoo Jeong", "M").type == "Manager"
-    assert create_employee("Minwoo Jeong", "S").type == "Salesperson"
+    assert create_engineer("Minwoo Jeong").type == "Engineer"
+    assert create_manager("Minwoo Jeong").type == "Manager"
+    assert create_salesperson("Minwoo Jeong").type == "Salesperson"
