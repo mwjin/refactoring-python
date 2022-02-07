ae4db2f71cf1fb24d1ac85d52bc9bce93822bf77
Minwoo Jeong <jeongmwj@gmail.com>
2022-02-07 12:52:26 +0900

    Remove the `ChargeCalculator` class

--- Chapter11/10-replace-command-with-function/charge.py
+++ Chapter11/10-replace-command-with-function/charge.py
@@ -1,12 +1,3 @@
-class ChargeCalculator:
-    def __init__(self, customer, usage, provider):
-        pass
-
-    def charge(self, customer, usage, provider):
-        base_charge = customer.base_rate * usage
-        return base_charge + provider.connection_charge
-
-
 def get_month_charge(customer, usage, provider):
     month_charge = charge(customer, usage, provider)
     return month_charge
