92566d02357fb0c1ff823ff303b88817bb11efb1
Minwoo Jeong <jeongmwj@gmail.com>
2022-02-07 12:14:55 +0900

    Move the `medical_exam` parameter into the constructor

--- Chapter11/09-replace-function-with-command/score.py
+++ Chapter11/09-replace-function-with-command/score.py
@@ -1,17 +1,18 @@
 def score(candidate, medical_exam, scoring_guide):
-    return Scorer(candidate).execute(medical_exam, scoring_guide)
+    return Scorer(candidate, medical_exam).execute(scoring_guide)
 
 
 class Scorer:
-    def __init__(self, candidate) -> None:
+    def __init__(self, candidate, medical_exam) -> None:
         self._candidate = candidate
+        self._medical_exam = medical_exam
 
-    def execute(self, medical_exam, scoring_guide):
+    def execute(self, scoring_guide):
         result = 0
         health_level = 0
         high_medical_risk_flag = False
 
-        if medical_exam.is_smoker:
+        if self._medical_exam.is_smoker:
             health_level += 10
             high_medical_risk_flag = True
 
