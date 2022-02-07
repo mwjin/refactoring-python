b4e1f9dc03063afc6b3d1bea1652afa940e62f1b
Minwoo Jeong <jeongmwj@gmail.com>
2022-02-07 12:16:11 +0900

    Move the `scoring_guide` parameter into the constructor

--- Chapter11/09-replace-function-with-command/score.py
+++ Chapter11/09-replace-function-with-command/score.py
@@ -1,13 +1,14 @@
 def score(candidate, medical_exam, scoring_guide):
-    return Scorer(candidate, medical_exam).execute(scoring_guide)
+    return Scorer(candidate, medical_exam, scoring_guide).execute()
 
 
 class Scorer:
-    def __init__(self, candidate, medical_exam) -> None:
+    def __init__(self, candidate, medical_exam, scoring_guide) -> None:
         self._candidate = candidate
         self._medical_exam = medical_exam
+        self._scoring_guide = scoring_guide
 
-    def execute(self, scoring_guide):
+    def execute(self):
         result = 0
         health_level = 0
         high_medical_risk_flag = False
@@ -17,7 +18,7 @@ class Scorer:
             high_medical_risk_flag = True
 
         certification_grade = "regular"
-        if scoring_guide.state_with_low_certification(
+        if self._scoring_guide.state_with_low_certification(
             self._candidate.origin_state
         ):
             certification_grade = "low"
