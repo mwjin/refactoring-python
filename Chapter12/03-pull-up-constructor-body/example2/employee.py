b5920997cd3129a306d9e1372be8dc95e84830ac
Minwoo Jeong <jeongmwj@gmail.com>
2022-02-14 12:55:41 +0900

    Create source code for the second example of 12.3

---
+++ Chapter12/03-pull-up-constructor-body/example2/employee.py
@@ -0,0 +1,10 @@
+class Employee:
+    def __init__(self, name) -> None:
+        self._name = name
+
+    @property
+    def is_privileged(self):
+        pass
+
+    def assign_car(self):
+        pass
---
+++ Chapter12/03-pull-up-constructor-body/example2/manager.py
@@ -0,0 +1,14 @@
+from employee import Employee
+
+
+class Manager(Employee):
+    def __init__(self, name, grade) -> None:
+        super().__init__(name)
+        self._grade = grade
+        # All the subclasses execute the below part
+        if self.is_privileged:
+            self.assign_car()
+
+    @property
+    def is_privileged(self):
+        return self._grade > 4
