963dd5560f15a138d7af5a830610e722663bc5f5
Minwoo Jeong <jeongmwj@gmail.com>
2022-02-14 12:24:58 +0900

    Implement `monthly_cost` in the `Party` class
    in order to notice that subclasses must implement this property

--- Chapter12/01-pull-up-method/party.py
+++ Chapter12/01-pull-up-method/party.py
@@ -1,4 +1,10 @@
 class Party:
     @property
+    def monthly_cost(self):
+        raise NotImplementedError(
+            "A subclass of 'Party' must implement this property."
+        )
+
+    @property
     def annual_cost(self):
         return self.monthly_cost * 12
