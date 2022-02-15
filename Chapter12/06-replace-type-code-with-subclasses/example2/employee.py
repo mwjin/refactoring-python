e0bf23a98fa8638f12d15c32e905ce1efef2dcfe
Minwoo Jeong <jeongmwj@gmail.com>
2022-02-15 12:48:11 +0900

    Replace the type code with object

--- Chapter12/06-replace-type-code-with-subclasses/example2/employee.py
+++ Chapter12/06-replace-type-code-with-subclasses/example2/employee.py
@@ -1,12 +1,24 @@
+class EmployeeType:
+    def __init__(self, value) -> None:
+        self._value = value
+
+    def __str__(self) -> str:
+        return self._value
+
+
 class Employee:
     def __init__(self, name, type) -> None:
         self._validate_type(type)
         self._name = name
-        self._type = type
+        self.type = type
 
     def _validate_type(self, type):
         if type not in ["engineer", "manager", "salesperson"]:
             raise ValueError(f'There is no "{type}" employee type.')
+
+    @property
+    def type_string(self):
+        return str(self._type)
 
     @property
     def type(self):
@@ -14,11 +26,11 @@ class Employee:
 
     @type.setter
     def type(self, value):
-        self._type = value
+        self._type = EmployeeType(value)
 
     @property
     def capitalized_type(self):
-        return self._type.capitalize()
+        return self.type_string.capitalize()
 
     def __str__(self) -> str:
         return f"{self._name} ({self.capitalized_type})"
