22d895605b3c63d093c47ff7bae761ea451aca40
Minwoo Jeong <jeongmwj@gmail.com>
2022-02-15 12:35:47 +0900

    Pull the `__str__` method down

--- Chapter12/06-replace-type-code-with-subclasses/example1/employee.py
+++ Chapter12/06-replace-type-code-with-subclasses/example1/employee.py
@@ -2,14 +2,14 @@ class Employee:
     def __init__(self, name) -> None:
         self._name = name
 
-    def __str__(self) -> str:
-        return f"{self._name} ({self.type})"
-
 
 class Engineer(Employee):
     @property
     def type(self):
         return "engineer"
+
+    def __str__(self) -> str:
+        return f"{self._name} ({self.type})"
 
 
 class Manager(Employee):
@@ -17,11 +17,17 @@ class Manager(Employee):
     def type(self):
         return "manager"
 
+    def __str__(self) -> str:
+        return f"{self._name} ({self.type})"
+
 
 class Salesperson(Employee):
     @property
     def type(self):
         return "salesperson"
+
+    def __str__(self) -> str:
+        return f"{self._name} ({self.type})"
 
 
 def create_employee(name, type):
