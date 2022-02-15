389da56ad34ce2ed32fa5f9d86bc14f4054f87f3
Minwoo Jeong <jeongmwj@gmail.com>
2022-02-15 12:33:11 +0900

    Change the declaration of the constructor

--- Chapter12/06-replace-type-code-with-subclasses/example1/employee.py
+++ Chapter12/06-replace-type-code-with-subclasses/example1/employee.py
@@ -1,5 +1,5 @@
 class Employee:
-    def __init__(self, name, type) -> None:
+    def __init__(self, name) -> None:
         self._name = name
 
     def __str__(self) -> str:
@@ -26,9 +26,9 @@ class Salesperson(Employee):
 
 def create_employee(name, type):
     if type == "engineer":
-        return Engineer(name, type)
+        return Engineer(name)
     if type == "manager":
-        return Manager(name, type)
+        return Manager(name)
     if type == "salesperson":
-        return Salesperson(name, type)
+        return Salesperson(name)
     raise ValueError(f'There is no "{type}" employee type.')
