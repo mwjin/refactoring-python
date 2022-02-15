0ee542d805d640f7bce1d0a0ba19a2e926fe6c26
Minwoo Jeong <jeongmwj@gmail.com>
2022-02-15 14:45:02 +0900

    Extract the function `is_male`

--- Chapter12/07-remove-subclass/client.py
+++ Chapter12/07-remove-subclass/client.py
@@ -2,4 +2,8 @@ from person import Male
 
 
 def get_number_of_males(people):
-    return len(list(filter(lambda p: isinstance(p, Male), people)))
+    return len(list(filter(lambda p: is_male(p), people)))
+
+
+def is_male(person):
+    return isinstance(person, Male)
