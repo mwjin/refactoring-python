567bad1c349c47a2d1c85fccf0ec7cf4f19148f7
Minwoo Jeong <jeongmwj@gmail.com>
2022-02-16 16:29:13 +0900

    Add transfer methods
    each of the methods corresponds to the method of superclass that uses in the subclass

--- Chapter12/11-replace-superclass-with-delegate/scroll.py
+++ Chapter12/11-replace-superclass-with-delegate/scroll.py
@@ -7,6 +7,17 @@ class Scroll(CatalogItem):
         self._catalog_item = CatalogItem(id, title, tags)
         self._last_cleaned = date_last_cleaned
 
+    @property
+    def id(self):
+        return self._catalog_item.id
+
+    @property
+    def title(self):
+        return self._catalog_item.title
+
+    def has_tag(self, tag):
+        return self._catalog_item.has_tag(tag)
+
     def needs_cleaning(self, target_date):
         threshold = 700 if self.has_tag("revered") else 1500
         return self.days_since_last_cleaning(target_date) > threshold
