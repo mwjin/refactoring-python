d358bede6740b7eebc7e6a7288cc288782e29c95
Minwoo Jeong <jeongmwj@gmail.com>
2022-02-16 16:31:27 +0900

    Remove the inheritance relationship

--- Chapter12/11-replace-superclass-with-delegate/scroll.py
+++ Chapter12/11-replace-superclass-with-delegate/scroll.py
@@ -1,9 +1,8 @@
 from catalog_item import CatalogItem
 
 
-class Scroll(CatalogItem):
+class Scroll:
     def __init__(self, id, title, tags, date_last_cleaned) -> None:
-        super().__init__(id, title, tags)
         self._catalog_item = CatalogItem(id, title, tags)
         self._last_cleaned = date_last_cleaned
 
